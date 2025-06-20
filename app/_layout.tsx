e2b3275f8664fbb516a48e9d8eda4da64fde04bf
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2025-06-20 21:10:47 +0300

    Tabs design updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { COLORS } from '../constants/theme';
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'COLORS.background' }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
         <Stack screenOptions={{ headerShown: false }} />
       </SafeAreaView>
     </SafeAreaProvider>
